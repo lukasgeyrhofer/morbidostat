483d1aa381349a5d53887b2d368d4285d4cc3e55
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-08-01 22:53:40 -0700

    working version of plate reader

--- platereader.py
+++ platereader.py
@@ -3,37 +3,84 @@
 import numpy as np
 import argparse
 import openpyxl
+import cairo
+
+# rows for starting values
+OD_start_row  = 28
+CFP_start_row = 59 
+GFP_start_row = 90 
+mC_start_row  = 121
+
+# starting column, usually B
+starting_column = ord('B')
+
+# plate dimensions
+platex = 12
+platey = 8
+
+def setOVERtoMAX(value):
+    if value == "OVER":
+        return 65536
+    else:
+        return value
+
+def rescale(values):
+    return (values - np.min(values))/(np.max(values) - np.min(values))
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infile",required=True,default=None)
+parser.add_argument("-S","--sheets",nargs="*",default=None)
+parser.add_argument("-o","--outbasename",type=str,default="")
+parser.add_argument("-p","--pixelsize",type=int,default = 20)
 args = parser.parse_args()
 
+try:
+    data = openpyxl.load_workbook(args.infile)
+except:
+    raise IOError("could not open file")
 
-data = openpyxl.load_workbook(args.infile)
-
-print(data.get_sheet_names())
-
-OD_start_column = 28
-CFP_start_column = 59 # ['B59', 'M66']
-GFP_start_column = 90 # ['B90', 'M97']
-mC_start_column  = 121 # ['B121','M128']
 
 for sheet in data:
-    try:
-        od  = np.array([[sheet['{}{}'.format(chr(66+i),OD_start_column  + j)].value for i in range(12)] for j in range(8) ],dtype=np.float64 )
-        cfp = np.array([[sheet['{}{}'.format(chr(66+i),CFP_start_column + j)].value for i in range(12)] for j in range(8) ],dtype=np.float64 )
-        gfp = np.array([[sheet['{}{}'.format(chr(66+i),GFP_start_column + j)].value for i in range(12)] for j in range(8) ],dtype=np.float64 )
+    if (args.sheets is None) or (sheet.title in args.sheets):
         
-        # rescale
-        
-        od = (od - np.min(od))/(np.max(od) - np.min(od))
-        
-        print(od)
-        
-        
-        #mC  = np.array([[sheet['{}{}'.format(chr(66+i),mC_start_column  + j)].value for i in range(12)] for j in range(8) ],dtype=np.float64 )
         print(sheet.title)
-        #print(od,cfp,gfp)
-    except:
-        continue
+        
+        # read data from Excel file and generate numpy array
+        # values are accessed by 'sheet["B28"].value' (where CellID "B28", ... is generated in for loops)
+        OD  = np.array([[setOVERtoMAX(sheet['{}{}'.format(chr(starting_column+i),OD_start_row  + j)].value) for i in range(platex)] for j in range(platey) ],dtype=np.float64 )
+        CFP = np.array([[setOVERtoMAX(sheet['{}{}'.format(chr(starting_column+i),CFP_start_row + j)].value) for i in range(platex)] for j in range(platey) ],dtype=np.float64 )
+        GFP = np.array([[setOVERtoMAX(sheet['{}{}'.format(chr(starting_column+i),GFP_start_row + j)].value) for i in range(platex)] for j in range(platey) ],dtype=np.float64 )
+        mC  = np.array([[setOVERtoMAX(sheet['{}{}'.format(chr(starting_column+i),mC_start_row  + j)].value) for i in range(platex)] for j in range(platey) ],dtype=np.float64 )
+        
+        # rescale, range is now [0:1]
+        ODr  = rescale(OD)
+        CFPr = rescale(CFP)
+        GFPr = rescale(GFP)
+        mCr  = rescale(mC)
+        
+        # generate figure
+        CairoImage = cairo.ImageSurface(cairo.FORMAT_ARGB32,platex * args.pixelsize,4 * platey * args.pixelsize)
+        context    = cairo.Context(CairoImage)
+        
+        for x in range(platex):
+            for y in range(platey):
+                context.set_source_rgb(ODr[y,x],ODr[y,x],ODr[y,x])
+                context.rectangle(x*args.pixelsize,(y + 0 * platey) * args.pixelsize,args.pixelsize,args.pixelsize)
+                context.fill()
+                
+                context.set_source_rgb(0,0,CFPr[y,x])
+                context.rectangle(x*args.pixelsize,(y + 1 * platey) * args.pixelsize,args.pixelsize,args.pixelsize)
+                context.fill()
+                
+                context.set_source_rgb(0,GFPr[y,x],0)
+                context.rectangle(x*args.pixelsize,(y + 2 * platey) * args.pixelsize,args.pixelsize,args.pixelsize)
+                context.fill()
+                
+                context.set_source_rgb(mCr[y,x],0,0)
+                context.rectangle(x*args.pixelsize,(y + 3 * platey) * args.pixelsize,args.pixelsize,args.pixelsize)
+                context.fill()
+                
+        CairoImage.write_to_png(args.outbasename + sheet.title + '.png')
+        
+        
