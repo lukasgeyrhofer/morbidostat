49de9ffefb8280c47e9467eda0a133c51ee3faa1
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-08-13 15:26:39 -0700

    allow multiple Excel files, delete debug msgs

--- platereader_assay.py
+++ platereader_assay.py
@@ -50,16 +50,18 @@ def AddRow(alldata,newdata):
 
 
 def splitCellID(cellID):
-    numbers = [str(i) for i in range(10)]
-    letter = ''
-    number = ''
-    for c in cellID:
-        if c in numbers:
-            number += c
-        else:
-            letter += c
-    return letter,number
-    
+    if not cellID is None:
+        numbers = [str(i) for i in range(10)]
+        letter = ''
+        number = ''
+        for c in cellID:
+            if c in numbers:
+                number += c
+            else:
+                letter += c
+        return letter,number
+    else:
+        return None
 
 # ===================================================== #
 # actual progam                                         #
@@ -67,34 +69,38 @@ def splitCellID(cellID):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--infile",required=True,default=None)
+    parser.add_argument("-i","--infiles",required=True,default=None,nargs="*")
     parser.add_argument("-o","--outfile",default="out",type=str)
     parser.add_argument("-B","--backgroundcolumn",default=[],type=str,nargs="*")
     parser.add_argument("-b","--backgroundrows",default = [],type=str,nargs="*")
     parser.add_argument("-O","--backgroundoutfile",default="background.histogram.txt",type=str)
     args = parser.parse_args()
 
-    # open Excel file
-    try:
-        data = openpyxl.load_workbook(args.infile)
-    except:
-        raise IOError("could not open file")
 
     assay = None
-    for sheet in data:
-        cellIDs = np.array([splitCellID(sheet[convert(data_starting_column + j + 2,id_starting_row)].value) for j in range(platex * platey)])
-        cell = convert(time_starting_column,time_starting_row)
-        timestr = sheet[cell].value
-        t_start = float((dateutil.parser.parse(timestr)).strftime("%s"))
-        if t_start > 0:
-            print("reading data from sheet '{}': starttime = {}".format(sheet.title,t_start),file=sys.stderr)
-            i = 0
-            while not sheet[convert(data_starting_column,data_starting_row + i)].value is None:
-                newrow         = np.array([float(sheet[convert(data_starting_column + j,data_starting_row + i)].value) for j in range(platex * platey + 2)]) # +2 for time and temp columns
-                newrow_time    = np.zeros(len(newrow))
-                newrow_time[0] = t_start
-                assay          = AddRow(assay,newrow+newrow_time)
-                i += 1
+    cellIDs = None
+    for fn in args.infiles:
+        # open Excel file
+        try:
+            data = openpyxl.load_workbook(fn)
+        except:
+            raise IOError("could not open file")
+
+        for sheet in data:
+            timestr = sheet[convert(time_starting_column,time_starting_row)].value
+            if not timestr is None:
+                t_start = float((dateutil.parser.parse(timestr)).strftime("%s"))
+                if t_start > 0:
+                    if cellIDs is None:
+                        cellIDs = np.array([splitCellID(sheet[convert(data_starting_column + j + 2,id_starting_row)].value) for j in range(platex * platey)])
+                    print("reading data from file '{}', sheet '{}': starttime = {}".format(fn,sheet.title,t_start),file=sys.stderr)
+                    i = 0
+                    while not sheet[convert(data_starting_column,data_starting_row + i)].value is None:
+                        newrow         = np.array([float(sheet[convert(data_starting_column + j,data_starting_row + i)].value) for j in range(platex * platey + 2)]) # +2 for time and temp columns
+                        newrow_time    = np.zeros(len(newrow))
+                        newrow_time[0] = t_start
+                        assay          = AddRow(assay,newrow+newrow_time)
+                        i += 1
 
     # rescale time
     assay[:,0] -= np.min(assay[:,0])
@@ -105,19 +111,17 @@ def main():
         backgroundvalues = np.array([])
         for well in range(platex * platey):
             if (len(args.backgroundcolumn) >= 1):
-                if (cellIDs[well,0] in args.backgroundcolumn):
+                if (cellIDs[well][0] in args.backgroundcolumn):
                     backgroundvalues = np.concatenate([backgroundvalues,assay[:,well+2]])
-                    print(cellIDs[well])
             if (len(args.backgroundrows) >= 1):
-                if (cellIDs[well,1] in args.backgroundrows):
-                    print(cellIDs[well])
+                if (cellIDs[well][1] in args.backgroundrows):
                     backgroundvalues = np.concatenate([backgroundvalues,assay[:,well+2]])
-        print(backgroundvalues)
-        assay[:,2:] = np.abs(assay[:,2:] - np.median(backgroundvalues))
-        h,b = np.histogram(backgroundvalues,bins = 80,range=[.02,.1])
-        
-        b = b[:-1] + .5*np.diff(b)
-        np.savetxt(args.backgroundoutfile,np.transpose([b,h]))
+        if len(backgroundvalues) > 0:
+            assay[:,2:] = np.abs(assay[:,2:] - np.median(backgroundvalues))
+            h,b = np.histogram(backgroundvalues,bins = 80,range=[.02,.1])
+            
+            b = b[:-1] + .5*np.diff(b)
+            np.savetxt(args.backgroundoutfile,np.transpose([b,h]))
         
     
     np.savetxt(args.outfile,assay)
